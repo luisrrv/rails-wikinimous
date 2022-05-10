1527a1cf7896bf68a3e762ddda5a32453ecc8f04
luis rodriguez <rodluisr@gmail.com>
2022-05-10 16:51:50 +0900

    fully working anonymous wiki completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Article.new(title: Faker::Quotes::Chiquito.expression, content: Faker::Quotes::Chiquito.joke).save
+end
+
+puts "created #{Article.count} articles"
